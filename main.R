e5345e23b587c747e5f807b43195ff7b8a2ece43
chararchter <lentenis@inbox.lv>
2018-04-02 21:43:10 +0300

    Koda arhitektūra

--- main.R
+++ main.R
@@ -1,3 +1,36 @@
 getwd()
 setwd("/home/vika/Documents/uni/4sem/LAB_4/4-Gaismas-interference/4data")
 getwd()
+
+alldata = list.files(pattern="*.csv")
+
+csvInput = function(filename){
+#ielasa .csv failu
+}
+
+filenameInterpret = function(filename){
+#ielasa filename
+#interpretē no filename nosaukuma eksperimenta parametrus
+#output: lambda, color, a, d, distance, mērījums pēc kārtas	
+}
+
+destroyTrash = function(vertibafilenamemainigajam){
+# piešķir jaunu mainīgo relevantajām kolonām vai
+# piešķir jaunu vērtību esošajiem mainīgajiem (tikai šīs 2 kolonas)
+# relativeIntensity = data$relativeIntensity
+# position = data$position
+}
+
+plot = function(){
+# input ir destroyTrash output
+# taisa grafiku un splainus
+}
+
+magicBox = function(){
+# this one calculates 3 lokālos maksimumus kur pirmais ir centra maksimums, un tad ir pārējie maksimumi
+}
+
+
+# for (i in 1:length(data)){
+	# izsauc visas funkcijas
+# }
