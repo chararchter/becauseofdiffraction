683e9b1bc5cb40705f7b309e3c617c3a82619c04
chararchter <lentenis@inbox.lv>
2018-04-09 17:55:32 +0300

    Work in process to make functions work together

--- main.R
+++ main.R
@@ -3,14 +3,14 @@ setwd("/home/vika/Documents/uni/4sem/LAB_4/4-Gaismas-interference/4data")
 getwd()
 
 alldata = list.files(pattern="*.csv")
-filename = "a004b025r340_1.csv"
+# filename = "a004b025r340_1.csv"
 
-csvInput = function(){
+csvInput = function(i){
 #ielasa .csv failu
 colNames = c('dateTime', 'Time', 'lightIntensity', 'relativeIntensity', 'angle', 'angularVelocity',
 	'angularAcceleration', 'position', 'velocity', 'acceleration')
 # alldata[i] vietā ielikt i-to lista alldata elementu
-# filename = read.csv(alldata[i], skip = 2, header = FALSE, col.names = colNames, sep = ";")
+filename = read.csv(alldata[i], skip = 2, header = FALSE, col.names = colNames, sep = ";")
 }
 
 patternSearch = function(filename, parameter){
@@ -39,7 +39,7 @@ filenameInterpret = function(filename){
 	return(c(color, ai, bi, distance, turn))
 }
 
-destroyTrash = function(vertibafilenamemainigajam){
+destroyTrash = function(filename){
 # input: variable - filename; value - filename ielasītie dati
 # output: divi vektori - pozīcija un relativeIntensity
 # piešķir jaunu mainīgo relevantajām kolonām vai piešķir jaunu vērtību esošajiem mainīgajiem (tikai šīs 2 kolonas)
@@ -52,35 +52,67 @@ relativeIntensity = (filename[, 'relativeIntensity'])
 return(c(position, relativeIntensity))
 }
 
-plot = function(){
+plot = function(i){
 # input ir destroyTrash output
-output = destroyTrash()
-x = output[1]
-y = output[2]
-splains = smooth.spline(x, y)
-plot.new()
-jpeg(paste('rplot', toString(filename), '.jpeg', sep=""), width = 900, height = 500, units = "px", pointsize = 10)
-plot(x, y, pch=20, xlim =c(0,3500), col = "gray25", xlab = "Position",
-	ylab="Relative intensity", cex.axis = 1.5, cex.lab=1.5)
-lines(splains, col = "blue")
-title(main = 'Junga dubultsprauga', cex.main = 2, font.main= 4, col.main= "black")
-abline(v=(seq(0,3500,100)), col="burlywood4", lty="dotted")
-abline(h=(seq(0,12,0.5)), col="burlywood2", lty="dotted")
-dev.off()
+# output = destroyTrash(filename)
+# x = output[1]
+# y = output[2]
+# # print(head(output))
+# print(output)
+
+position = (filename[, 'position'])
+relativeIntensity = (filename[, 'relativeIntensity'])
+# create new dataset without missing data
+newdata <- na.omit(position) 
+print(length(newdata))
+print(length(relativeIntensity))
+
+print(is.na(relativeIntensity))
+# print(newdata)
+# print(position)
+# print(head(filename))
+# print(filename[, 'position'])
+# print(filename[, 'relativeIntensity'])
+# print(relativeIntensity)
+# print(x)
+# print(y)
+# splains = smooth.spline(x, y)
+# plot.new()
+# jpeg(paste('rplot', toString(i), '.jpeg', sep=""), width = 900, height = 500, units = "px", pointsize = 10)
+# plot(x, y, pch=20, xlim =c(0,3500), col = "gray25", xlab = "Position",
+# 	ylab="Relative intensity", cex.axis = 1.5, cex.lab=1.5)
+# lines(splains, col = "blue")
+# title(main = 'Junga dubultsprauga', cex.main = 2, font.main= 4, col.main= "black")
+# abline(v=(seq(0,3500,100)), col="burlywood4", lty="dotted")
+# abline(h=(seq(0,12,0.5)), col="burlywood2", lty="dotted")
+# dev.off()
 }
 
 magicBox = function(){
 # this one calculates 3 lokālos maksimumus kur pirmais ir centra maksimums, un tad ir pārējie maksimumi
+# atrod the maksimumu
+# apgriež tur pa vidu
+
+}
+
+# i in 1:length(data)
+for (i in 1:2){
+	# izsauc visas funkcijas
+	filename = csvInput(i)
+	interpretation = filenameInterpret(alldata[i])
+	allineed = plot(i)
 }
 
 
-# for (i in 1:length(data)){
-	# izsauc visas funkcijas
-# }
+# filename = csvInput(1)
+# interpretation = filenameInterpret(filename)
 
+# print(head(filename))
+# print(head(alldata[1]))
+# print(interpretation)
 
-list = filenameInterpret(filename)
-names(list) <- c("color", "ai", "bi", "turn")
+# list = filenameInterpret(filename)
+# names(list) <- c("color", "ai", "bi", "turn")
 
-print(list)
-names(list)
+# print(list)
+# names(list)
